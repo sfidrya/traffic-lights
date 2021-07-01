28fca50b518a3cb4875746326a6d7c82d6299e3a
Svetlana Fidrya <sf@zabiyaka.com>
2021-07-01 11:59:07 +0300

    implement changing lights in autoswitch mode. but it's changing in circle (green after red, not yellow)

--- src/trafficLightsAll.tsx
+++ src/trafficLightsAll.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 //import './trafficLightsAll.css';
 import {TrafficLights} from './trafficLights'
 import {LightMenu} from './menu/LightMenu'
@@ -9,6 +9,29 @@ export const TrafficLightsAll = () => {
   const [activeColor, setActiveColor] = useState<Color | 'none'>('red')
   const [isBlinking, setIsBlinking] = useState(false)
   const [isAutoSwitch, setIsAutoSwitch] = useState(false)
+  const [autoSwitchColor, setAutoSwitchColor] = useState<Color> ('red')
+
+  useEffect(() => {
+    function autoSwitch() {
+      if (autoSwitchColor === 'green') {
+       setAutoSwitchColor('yellow')
+      }  else if (autoSwitchColor === 'yellow') {
+        setAutoSwitchColor ('red') 
+      } else {
+        setAutoSwitchColor('green')
+      }
+      console.log('autoSwitch worked')
+    }
+    if (isAutoSwitch) {
+      const autoSwitchTimer = setTimeout(autoSwitch, 3000)
+     /* return function cleanup() {
+        clearTimeout(autoSwitchTimer)
+        console.log('CLEARED TIMEOUT')
+      }*/
+    } 
+  }
+  , 
+  [autoSwitchColor, isAutoSwitch])
   
 
   const handleActiveColorChange = useCallback((newValue: Color | 'none') => {
@@ -21,12 +44,14 @@ export const TrafficLightsAll = () => {
 
   const handleIsAutoSwitchChange = useCallback((newValue: boolean) => {
     setIsAutoSwitch(newValue)
+    setAutoSwitchColor('red')
   }, [])
 
 
   return (
     <>
       <TrafficLights activeColor={activeColor} isBlinking={isBlinking}/>
+      <TrafficLights activeColor={isAutoSwitch ? autoSwitchColor : activeColor} isBlinking={isBlinking}/>
       <LightMenu 
         activeColor={activeColor} 
         isBlinking={isBlinking} 
